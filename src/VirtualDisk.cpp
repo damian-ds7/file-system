d20ba215c1cd53fd6662683ea00ef679a08b5d67
Damian D'Souza <damian.dsouza.stud@pw.edu.pl>
2025-01-17 04:59:15 +0100

    implemented basic functions

--- src/VirtualDisk.cpp
+++ src/VirtualDisk.cpp
@@ -4,24 +4,32 @@
 #include <stdexcept>
 #include <filesystem>
 #include <cstring>
+#include <utility>
 
 #include "Directory.h"
 #include "typedefs.h"
 #include "getTimestamp.h"
 
 void VirtualDisk::createRootDirectory() {
-    auto node = INode::rootNode();
-    auto blockIdx = blocksBitmap.findBlocks(1)[0];
-    auto nodeIdx = iNodesBitmap.findINode();
+    const uint32_t blockIdx = blocksBitmap.findBlocks(1)[0];
+    const uint16_t nodeIdx = iNodesBitmap.findINode();
 
-    node.blocks[0] = blockIdx;
+    const INode node = INode::directoryNode(blockIdx);
+
     iNodes[nodeIdx] = node;
     const auto directory = Directory(nodeIdx, nodeIdx);
     std::memcpy(blocks[blockIdx].data, &directory, sizeof(directory));
     ++superBlock.fileCount;
 }
 
-VirtualDisk::VirtualDisk(size_t diskSize) {
+Directory VirtualDisk::getDirectory(const uint32_t iNodeIndex) const {
+    Directory directory;
+    const auto blockIdx = iNodes[iNodeIndex].blocks[0];
+    std::memcpy(&directory, blocks[blockIdx].data, sizeof(Directory));
+    return directory;
+}
+
+VirtualDisk::VirtualDisk(size_t diskSize, std::string diskName) : diskName(std::move(diskName)) {
     const size_t iNodeCount = diskSize / MAX_FILE_SIZE;
 
     if (iNodeCount < 1) {
@@ -43,25 +51,27 @@ VirtualDisk::VirtualDisk(size_t diskSize) {
 
     iNodesBitmap = INodesBitmap(iNodeCount);
     blocksBitmap = BlocksBitmap(iNodeCount * INODE_BLOCKS);
-    iNodesBitmap.findINode();
-    blocksBitmap.findBlocks(10);
 
     blocks = std::vector<Block>(blockCount);
 
     createRootDirectory();
 }
 
-void VirtualDisk::saveToFile(const std::string& filename) {
-    std::ofstream file(filename, std::ios::binary);
+void VirtualDisk::saveToFile() {
+    std::ofstream file(diskName, std::ios::binary);
 
     if (!file) {
         throw std::runtime_error("Failed to open file for writing");
     }
+    superBlock.fileCount = 100;
+
+    auto iNodesData = iNodesBitmap.getBitmap();
+    auto blocksData = blocksBitmap.getBitmap();
 
     file.write(reinterpret_cast<char*>(&superBlock), sizeof(SuperBlock));
     file.write(reinterpret_cast<char*>(iNodes.data()), sizeof(INode) * iNodes.size());
-    file.write(reinterpret_cast<char*>(&iNodesBitmap), sizeof(INodesBitmap));
-    file.write(reinterpret_cast<char*>(&blocksBitmap), sizeof(BlocksBitmap));
+    file.write(reinterpret_cast<char*>(iNodesData.data()), sizeof(uint8_t) * iNodesData.size());
+    file.write(reinterpret_cast<char*>(blocksData.data()), sizeof(uint8_t) * blocksData.size());
     file.write(reinterpret_cast<char*>(blocks.data()), sizeof(Block) * blocks.size());
     file.close();
 }
@@ -72,14 +82,21 @@ VirtualDisk VirtualDisk::loadFromFile(const std::string& filename) {
         throw std::runtime_error("Failed to open file for reading");
     }
 
-    VirtualDisk virtualDisk{};
-    auto iNodeCount = virtualDisk.superBlock.blockCount / INODE_BLOCKS;
-    auto blockCount = virtualDisk.superBlock.blockCount;
+    SuperBlock superBlock{};
+    file.read(reinterpret_cast<char*>(&superBlock), sizeof(SuperBlock));
 
-    file.read(reinterpret_cast<char*>(&virtualDisk.superBlock), sizeof(SuperBlock));
+    VirtualDisk virtualDisk(superBlock.diskSize, filename);
+    virtualDisk.superBlock = superBlock;
+
+    auto iNodeCount = superBlock.blockCount / INODE_BLOCKS;
+    auto blockCount = superBlock.blockCount;
+
+    auto iNodesData = virtualDisk.iNodesBitmap.getBitmap();
+    auto blocksData = virtualDisk.blocksBitmap.getBitmap();
+
     file.read(reinterpret_cast<char*>(virtualDisk.iNodes.data()), sizeof(INode) * iNodeCount);
-    file.read(reinterpret_cast<char*>(&virtualDisk.iNodesBitmap), sizeof(INodesBitmap));
-    file.read(reinterpret_cast<char*>(&virtualDisk.blocksBitmap), sizeof(BlocksBitmap));
+    file.read(reinterpret_cast<char*>(iNodesData.data()), sizeof(uint8_t) * iNodesData.size());
+    file.read(reinterpret_cast<char*>(blocksData.data()), sizeof(uint8_t) * blocksData.size());
     file.read(reinterpret_cast<char*>(virtualDisk.blocks.data()), sizeof(Block) * blockCount);
     file.close();
     return virtualDisk;
@@ -90,9 +107,14 @@ bytesVector VirtualDisk::getBytesFromFile(const std::string& filename) {
     if (!file) {
         throw std::runtime_error("Failed to open file for reading");
     }
-    const auto length = std::filesystem::file_size(filename);
-    bytesVector bytes;
-    file.read(reinterpret_cast<char*>(bytes.data()), static_cast<long>(length));
+
+    file.seekg(0, std::ios::end);
+    size_t size = file.tellg();
+    file.seekg(0);
+
+    bytesVector bytes(size);
+    file.read(reinterpret_cast<char*>(bytes.data()), size);
+    file.close();
     return bytes;
 }
 
@@ -105,14 +127,33 @@ uint16_t VirtualDisk::getBlockCountFromFile(const bytesVector& bytes) {
 }
 
 void VirtualDisk::saveDataToBlocks(const bytesVector& bytes, const std::vector<uint32_t>& blockIndices) {
-    auto blockStart = bytes.begin();
-    auto blockEnd = bytes.begin() + BLOCK_SIZE;
-    for (const auto& block : blockIndices) {
-        blockEnd = std::max(blockEnd, bytes.end());
-        std::copy(blockStart, blockEnd, blocks[block].data);
-        blockStart = blockEnd + 1;
-        blockEnd += BLOCK_SIZE;
+    auto fileSize = bytes.size();
+    auto fullBlockCount = fileSize / BLOCK_SIZE;
+
+    for (size_t i = 0; i < fullBlockCount; ++i) {
+        auto blockIdx = blockIndices[i];
+        auto block = blocks[blockIdx].data;
+        auto offset = i * BLOCK_SIZE;
+        std::copy(bytes.begin() + offset, bytes.begin() + offset + BLOCK_SIZE, block);
     }
+
+    if (fullBlockCount != blockIndices.size()) {
+        auto blockIdx = blockIndices[fullBlockCount];
+        auto block = blocks[blockIdx].data;
+        auto offset = fullBlockCount * BLOCK_SIZE;
+        auto sizeToCopy = fileSize - offset;
+        std::copy(bytes.begin() + offset, bytes.begin() + offset + sizeToCopy, block);
+    }
+
+}
+
+Directory VirtualDisk::getCurrentDirectory() const {
+    return getDirectory(currentNode);
+}
+
+void VirtualDisk::updateCurrentDirectory(const Directory& directory) {
+    const auto blockIdx = iNodes[currentNode].blocks[0];
+    std::memcpy(&blocks[blockIdx].data, &directory, sizeof(Directory));
 }
 
 void VirtualDisk::copyFromSystemDrive(const std::string& sourceFileName, const std::string& targetFileName) {
@@ -138,4 +179,83 @@ void VirtualDisk::copyFromSystemDrive(const std::string& sourceFileName, const s
 
     iNodes[iNodeIndex] = fileNode;
     saveDataToBlocks(bytes, blocksIndices);
+
+    auto currentDir = getCurrentDirectory();
+    currentDir.addEntry(iNodeIndex, targetFileName);
+    updateCurrentDirectory(currentDir);
+}
+
+bytesVector VirtualDisk::getFileContents(uint16_t iNodeIdx) {
+    const auto node = iNodes[iNodeIdx];
+    bytesVector contents(node.fileSize);
+    const auto trailingBytesSize = node.fileSize % BLOCK_SIZE;
+    const auto blockCount = node.fileSize / BLOCK_SIZE;
+    int i = 0;
+
+    for (; i < blockCount; ++i) {
+        std::copy(blocks[node.blocks[i]].data, blocks[node.blocks[i]].data + BLOCK_SIZE,
+                  contents.begin() + i * BLOCK_SIZE);
+    }
+
+    if (trailingBytesSize > 0) {
+        std::copy(blocks[node.blocks[i]].data, blocks[node.blocks[i]].data + trailingBytesSize,
+                  contents.begin() + i * BLOCK_SIZE);
+    }
+
+    return contents;
+}
+
+void VirtualDisk::copyToSystemDrive(const std::string& sourceFileName, const std::string& targetFileName) {
+    auto currentDir = getCurrentDirectory();
+    const DirectoryEntry* entry = currentDir.getEntry(sourceFileName);
+    bytesVector bytes = getFileContents(entry->iNodeNumber);
+    auto fileSize = iNodes[entry->iNodeNumber].fileSize;
+
+    std::ofstream file(targetFileName, std::ios::binary);
+
+    if (!file) {
+        throw std::runtime_error("Failed to open file for writing");
+    }
+
+    file.write(reinterpret_cast<char*>(bytes.data()), fileSize);
+    file.close();
+}
+
+void VirtualDisk::makeDirectory(const std::string& name) {
+    if (superBlock.fileCount == (superBlock.blockCount / INODE_BLOCKS)) {
+        throw std::runtime_error("No space on drive");
+    }
+
+    auto currentDir = getCurrentDirectory();
+    if (currentDir.getEntryCount() == DIRECTORY_SIZE) {
+        throw std::runtime_error("No space for new entry in current directory");
+    }
+
+    const auto iNodeIdx = iNodesBitmap.findINode();
+    const auto blockIdx = blocksBitmap.findBlocks(1)[0];
+
+    const INode node = INode::directoryNode(blockIdx);
+    iNodes[iNodeIdx] = node;
+
+    currentDir.addEntry(iNodeIdx, name);
+    updateCurrentDirectory(currentDir);
+}
+
+void VirtualDisk::removeDirectory(const std::string& name) {
+    auto currentDir = getCurrentDirectory();
+    const DirectoryEntry* entryToRemove = currentDir.getEntry(name);
+    auto directory = getDirectory(entryToRemove->iNodeNumber);
+
+    for (auto& entry : directory.entries) {
+        auto filename = entry.filename;
+        if (strcmp(filename, ".") != 0 && strcmp(filename, "..") != 0 && strcmp(filename, "\0") != 0) {
+            throw std::runtime_error("Directory is not empty");
+        }
+    }
+
+    iNodesBitmap.freeINode(entryToRemove->iNodeNumber);
+    blocksBitmap.freeBlocks(iNodes[entryToRemove->iNodeNumber].blocks[0]);
+
+    currentDir.removeEntry(entryToRemove->iNodeNumber);
+    updateCurrentDirectory(currentDir);
 }
--- src/VirtualDisk.h
+++ src/VirtualDisk.h
@@ -4,6 +4,7 @@
 #include "SuperBlock.h"
 #include "INodesBitmap.h"
 #include "BlocksBitmap.h"
+#include "Directory.h"
 #include "INode.h"
 
 class VirtualDisk {
@@ -15,21 +16,21 @@ class VirtualDisk {
 
     std::vector<Block> blocks;
 
-    unsigned int currentNode = 0;
+    std::string diskName;
+    uint32_t currentNode = 0;
 
     void createRootDirectory();
 
+    [[nodiscard]] Directory getDirectory(uint32_t iNodeIndex) const;
+
 public:
-    explicit VirtualDisk(size_t diskSize);
+    explicit VirtualDisk(size_t diskSize, std::string  diskName);
     VirtualDisk() = default;
-
-
 
     /**
      * Saves current disk instance to file
-     * @param filename name to save disk to
      */
-    void saveToFile(const std::string& filename);
+    void saveToFile();
 
     /**
      * Create disk from file
@@ -59,6 +60,9 @@ public:
      */
     void saveDataToBlocks(const bytesVector& bytes, const std::vector<uint32_t>& blockIndices);
 
+    [[nodiscard]] Directory getCurrentDirectory() const;
+    void updateCurrentDirectory(const Directory &directory);
+
     /**
      * Copy a file from system drive to virtual disk
      * @param sourceFileName name of the file to copy
@@ -67,11 +71,16 @@ public:
     void copyFromSystemDrive(const std::string& sourceFileName, const std::string& targetFileName);
 
     /**
+     * Return contents of file under given INode index as a vector of bytes
+     */
+    bytesVector getFileContents(uint16_t iNodeIdx);
+
+    /**
     * Copy a file from virtual to system drive
      * @param sourceFileName name of the file to copy
-     * @param TargetFileName name to save file as
+     * @param targetFileName name to save file as
      */
-    void copyToSystemDrive(const std::string& sourceFileName, const std::string& TargetFileName);
+    void copyToSystemDrive(const std::string& sourceFileName, const std::string& targetFileName);
 
     /**
      * Create a directory in current working directory
